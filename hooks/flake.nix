a7302f1b9b13ca46c663003d64f9b9ec45d956b6
valeratrades <valeratrades@gmail.com>
2025-01-08 17:10:05 +0000

    feat: hooks framework

--- hooks/flake.nix
+++ hooks/flake.nix
@@ -2,7 +2,6 @@
 	description = "Random tools for interfacing with github hooks";
 	outputs = { self }: {
 		appendCustom = ./append_custom.rs;
-		#treefmt = ./treefmt.tom;l
 		treefmt = ./treefmt.nix;
 	};
 }
