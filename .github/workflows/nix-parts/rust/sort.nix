d7be57382414a9f9479ae50cb862be4b5f5fdb06
valeratrades <valeratrades@gmail.com>
2025-01-17 00:05:22 +0000

    _

--- .github/workflows/nix-parts/rust/sort.nix
+++ .github/workflows/nix-parts/rust/sort.nix
@@ -1,24 +1,10 @@
+#HACK: at the current moment (2024/01/16) is slower than installing with binstall. However, the other thing is having some weird issues which I can't debug, so here we go with defaults.
 { ... }: {
   name = "Cargo Sorted";
   runs-on = "ubuntu-latest";
   steps = [
-    { uses = "actions/checkout@v4"; }
     {
-      name = "Installation";
-      uses = "taiki-e/install-action@v2";
-      "with".tool = "cargo-sort";
-    }
-    {
-      name = "Check if Cargo.toml is sorted";
-      run = ''
-				rm -rf .cargo # cargo-sort can get confused by `[target.x86_64-unknown-linux-gnu]` settings in .cargo/config.toml
-        cargo sort -wc
-        exit_code=$?
-        if [ $exit_code != 0 ]; then
-          echo "Cargo.toml is not sorted. Run \`cargo sort -w\` to fix it."
-          exit $exit_code
-        fi
-      '';
+      uses = "DevinR528/cargo-sort@v1.0.4";
     }
   ];
 }
