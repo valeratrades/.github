024a4974efdec31acf49cd676f3f5dda67d2caa2
valeratrades <valeratrades@gmail.com>
2025-01-30 16:09:05 +0000

    _

--- hooks/treefmt.nix
+++ hooks/treefmt.nix
@@ -13,5 +13,10 @@ pkgs,
 			options = ["--edition" "2024"];
 			includes = ["*.rs"];
 		};
+		rust-leptos = {
+			command = "leptosfmt";
+			options = ["--tab-spaces" "2" "--max-width" "120"];
+			includes = ["*.rs"];
+		};
 	};
 }
