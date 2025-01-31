ba933c62f2009eb16b76fc7f50866ab663ec9583
valeratrades <valeratrades@gmail.com>
2025-01-31 03:29:33 +0000

    _

--- hooks/treefmt.nix
+++ hooks/treefmt.nix
@@ -13,6 +13,7 @@ pkgs,
 			options = ["--edition" "2024"];
 			includes = ["*.rs"];
 		};
+		# Does not seem to work (treefmt feels VERY raw)
 		rust-leptos = {
 			command = "leptosfmt";
 			options = ["--tab-spaces" "2" "--max-width" "120"];
