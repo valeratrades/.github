35c69d6d938097452035db136953d9d35a3d19a6
valeratrades <valeratrades@gmail.com>
2025-02-06 18:31:37 +0000

    _

--- files/flake.nix
+++ files/flake.nix
@@ -2,7 +2,7 @@
 	description = "Project conf files";
 	outputs = { self }: {
 		licenses = {
-			blue_oak = builtins.readFile ./licenses/BlueOak_1-0-0.md;
+			blue_oak = ./licenses/BlueOak_1-0-0.md;
 		};
 	};
 }
