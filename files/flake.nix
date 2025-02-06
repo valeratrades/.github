a4547296321a0c1838916ecbc7415b54bbaa3648
valeratrades <valeratrades@gmail.com>
2025-02-06 22:16:05 +0000

    _

--- files/flake.nix
+++ files/flake.nix
@@ -2,8 +2,12 @@
 	description = "Project conf files";
 	outputs = { self }: {
 		licenses = {
-			blue_oak = ./licenses/BlueOak_1-0-0.md;
+			blue_oak = ./licenses/blue_oak.md;
 		};
+		rust = {
+			rustfmt.toml = ./rust/rustfmt.toml;
+		};
+		treefmt = ./treefmt.toml;
+		#TODO: gitignore: construct from base + each name from provided list
 	};
 }
-
