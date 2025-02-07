b1b0b827b47080714d784297ea2af0b62e897197
valeratrades <valeratrades@gmail.com>
2025-02-07 15:06:29 +0000

    _

--- files/default.nix
+++ files/default.nix
@@ -3,9 +3,9 @@
   licenses = {
     blue_oak = ./licenses/blue_oak.md;
   };
-	rust = {
-    rustfmt = ./rust/rustfmt.nix;
-  };
-	rustfmt = ./rust/rustfmt.nix; #dbg
+	#rust = {
+ #   rustfmt = ./rust/rustfmt.nix;
+ # };
+	#rustfmt = ./rust/rustfmt.nix; #dbg
   #TODO: gitignore: construct from base + each name from provided list
 }
