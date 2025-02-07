2818bf0c3dfad7150bb3f5a945df0b496d339af4
valeratrades <valeratrades@gmail.com>
2025-02-07 13:18:12 +0000

    _

--- flake.nix
+++ flake.nix
@@ -1,8 +1,35 @@
 {
-  outputs = { self, nixpkgs }: {
+  description = ''
+# Nix parts collection
+
+Collection of reusable Nix components.
+See individual component descriptions in their respective directories.'';
+
+  outputs = { self, nixpkgs }: let
+    parts = {
+      files = (import ./files { inherit nixpkgs; }).description;
+      hooks = (import ./hooks { inherit nixpkgs; }).description;
+      readme-fw = (import ./readme_fw { inherit nixpkgs; }).description;
+      workflows = (import ./.github/workflows/nix-parts { inherit nixpkgs; }).description;
+    };
+  in {
+    description = ''
+## Files
+${parts.files}
+
+## Hooks
+${parts.hooks}
+
+## Readme Framework
+${parts.readme-fw}
+
+## Workflows
+${parts.workflows}
+'';
+
     files = import ./files { inherit nixpkgs; };
-		hooks = import ./hooks { inherit nixpkgs; };
-		readme-fw = (import ./readme_fw { inherit nixpkgs; }).generator;
-		workflows = import ./.github/workflows/nix-parts { inherit nixpkgs; };
+    hooks = import ./hooks { inherit nixpkgs; };
+    readme-fw = (import ./readme_fw { inherit nixpkgs; }).generator;
+    workflows = import ./.github/workflows/nix-parts { inherit nixpkgs; };
   };
 }
