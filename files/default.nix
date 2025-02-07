894eeeea3350a532b9b25dc42e55eca7c849d7fa
valeratrades <valeratrades@gmail.com>
2025-02-07 12:43:05 +0000

    _

--- files/default.nix
+++ files/default.nix
@@ -1,13 +1,10 @@
-{
-	description = "Project conf files";
-	outputs = { self }: {
-		licenses = {
-			blue_oak = ./licenses/blue_oak.md;
-		};
-		rust = {
-			rustfmt.toml = ./rust/rustfmt.toml;
-		};
-		treefmt = ./treefmt.toml;
-		#TODO: gitignore: construct from base + each name from provided list
-	};
+{ pkgs ? import <nixpkgs> {} }: {
+  licenses = {
+    blue_oak = ./licenses/blue_oak.md;
+  };
+  rust = {
+    rustfmt.toml = ./rust/rustfmt.toml;
+  };
+  treefmt = ./treefmt.toml;
+  #TODO: gitignore: construct from base + each name from provided list
 }
