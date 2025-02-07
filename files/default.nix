2349f4194ef034caff8bf676001dd4f68e5698be
valeratrades <valeratrades@gmail.com>
2025-02-07 13:47:11 +0000

    _

--- files/default.nix
+++ files/default.nix
@@ -3,13 +3,9 @@
   licenses = {
     blue_oak = ./licenses/blue_oak.md;
   };
-  rust = {
-    #rustfmt = ./rust/rustfmt.toml;
-		rustfmt = let
-			rustfmtConf = (import ./rust/rustfmt.nix);
-			in
-			(nixpkgs.formats.toml {}).generate "rustfmt.toml" rustfmtConf;
-  };
+	rust = {
+    rustfmt = (nixpkgs.formats.toml {}).generate "rustfmt.toml" (import ./rust/rustfmt.nix);
+  }.rustfmt.outPath;
   treefmt = ./treefmt.toml;
   #TODO: gitignore: construct from base + each name from provided list
 }
