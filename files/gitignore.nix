4cf602eecc175176ee9d19c779b179f4b679f50a
valeratrades <valeratrades@gmail.com>
2025-02-10 12:40:09 +0000

    _

--- files/gitignore.nix
+++ files/gitignore.nix
@@ -1,14 +1,10 @@
 { pkgs, langs }: 
 let
   gitignore = {
-    shared = ./gitignore/.gitignore;
-    rs = ./gitignore/rs.gitignore;
-    go = ./gitignore/go.gitignore;
-    py = ./gitignore/py.gitignore;
+    shared = pkgs.runCommand "shared-gitignore" {} ''cat ${./gitignore/.gitignore} > $out'';
+    rs = pkgs.runCommand "rs-gitignore" {} ''cat ${./gitignore/rs.gitignore} > $out'';
+    go = pkgs.runCommand "go-gitignore" {} ''cat ${./gitignore/go.gitignore} > $out'';
+    py = pkgs.runCommand "py-gitignore" {} ''cat ${./gitignore/py.gitignore} > $out'';
   };
 in
-  pkgs.runCommand "combined-gitignore" {} ''
-    {
-      ${builtins.concatStringsSep "\n" (map (lang: "cat ${gitignore.${lang}}") (["shared"] ++ langs))}
-    } > $out
-  ''
+  builtins.concatStringsSep "\n" (map (lang: gitignore.${lang}) (["shared"] ++ langs))
