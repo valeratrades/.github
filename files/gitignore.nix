7af825e57552e0a035ec5595f93f3f89c3635118
valeratrades <valeratrades@gmail.com>
2025-02-10 11:53:34 +0000

    _

--- files/gitignore.nix
+++ files/gitignore.nix
@@ -1,16 +1,16 @@
 { pkgs, langs }: 
 let
   gitignore = {
-    shared = ./gitignore/.gitignore;
-    rs = ./gitignore/rs.gitignore;
-    go = ./gitignore/go.gitignore;
-    py = ./gitignore/py.gitignore;
+    shared = builtins.readFile ./gitignore/.gitignore;
+    rs = builtins.readFile ./gitignore/rs.gitignore;
+    go = builtins.readFile ./gitignore/go.gitignore;
+    py = builtins.readFile ./gitignore/py.gitignore;
   };
 
   combineGitignore = selected_langs: 
     let
 			all = [ "shared" ] ++ selected_langs;
-      joined = builtins.concatStringsSep "\n" (lang: (builtins.readFile gitignore.${lang})) all;
+      joined = builtins.concatStringsSep "\n" (lang: gitignore.${lang}) all;
     in
     pkgs.runCommand "" {} ''
       cat > $out <<'EOF'
