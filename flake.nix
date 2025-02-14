708eab65b5bb6fc16a948e783558ef233a4a6687
valeratrades <valeratrades@gmail.com>
2025-02-14 18:33:57 +0000

    _

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@ ${parts.workflows}
 
     files = import ./files { inherit nixpkgs; };
     hooks = import ./hooks { inherit nixpkgs; };
-    readme-fw = import ./readme_fw { pkgs = nixpkgs; };
+    readme-fw = ./readme_fw;
     workflows = import ./.github/workflows/nix-parts { inherit nixpkgs; };
   };
 }
