96041a2651c54f933b46396892602167f5337203
valeratrades <valeratrades@gmail.com>
2025-02-14 20:34:23 +0000

    _

--- .github/workflows/nix-parts/default.nix
+++ .github/workflows/nix-parts/default.nix
@@ -1,23 +1,59 @@
-{ nixpkgs }:
+{ pkgs, lastSupportedVersion, jobsErrors, jobsWarnings }:
+let
+  shared = {
+    base = ./shared/base.nix;
+    tokei = ./shared/tokei.nix;
+  };
+  rust = {
+    base = ./rust/base.nix;
+    tests = ./rust/tests.nix;
+    doc = ./rust/doc.nix;
+    miri = ./rust/miri.nix;
+    clippy = ./rust/clippy.nix;
+    machete = ./rust/machete.nix;
+    sort = ./rust/sort.nix;
+  };
+  go = {
+    tests = ./go/tests.nix;
+    gocritic = ./go/gocritic.nix;
+    security_audit = ./go/security_audit.nix;
+  };
+
+  constructJobs = paths:
+    let
+      imports = map (path:
+        if path == "rust.tests" then
+					import ${path} { inherit lastSupportedVersion; }
+        else
+					import ${path}
+      ) paths;
+    in
+    pkgs.lib.foldl pkgs.lib.recursiveUpdate { } imports;
+
+  base = {
+    on = {
+      push = { };
+      pull_request = { };
+      workflow_dispatch = { };
+    };
+  };
+
+in
 {
- shared = {
-   base = ./shared/base.nix;
+  errors = (pkgs.formats.yaml { }).generate "" (
+    pkgs.lib.recursiveUpdate base {
+      name = "Errors";
+      permissions = (import shared.base).permissions;
+      env = (import rust.base).env;
+      jobs = constructJobs jobsErrors;
+    }
+  );
 
-   tokei = ./shared/tokei.nix;
- };
- rust = {
-   base = ./rust/base.nix;
-
-   tests = ./rust/tests.nix;
-   doc = ./rust/doc.nix;
-   miri = ./rust/miri.nix;
-   clippy = ./rust/clippy.nix;
-   machete = ./rust/machete.nix;
-   sort = ./rust/sort.nix;
- };
- go = {
-   tests = ./go/tests.nix;
-   gocritic = ./go/gocritic.nix;
-   security_audit = ./go/security_audit.nix;
- };
+  warnings = (pkgs.formats.yaml { }).generate "" (
+    pkgs.lib.recursiveUpdate base {
+      name = "Warnings";
+      permissions = (import shared.base).permissions;
+      jobs = constructJobs jobsWarnings;
+    }
+  );
 }
--- flake.nix
+++ flake.nix
@@ -30,6 +30,7 @@ ${parts.workflows}
     files = import ./files { inherit nixpkgs; };
     hooks = import ./hooks { inherit nixpkgs; };
     readme-fw = import ./readme_fw;
-    workflows = import ./.github/workflows/nix-parts { inherit nixpkgs; };
+    workflows = import ./.github/workflows/nix-parts { inherit nixpkgs; }; #dbg: superseded by `ci`
+		ci = ./.github/workflows/nix-parts;
   };
 }
