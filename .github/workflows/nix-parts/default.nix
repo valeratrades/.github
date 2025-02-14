2769a6773eb1c9d839fb0cd48cf3be2e820faebe
valeratrades <valeratrades@gmail.com>
2025-02-14 21:11:56 +0000

    _

--- .github/workflows/nix-parts/default.nix
+++ .github/workflows/nix-parts/default.nix
@@ -1,45 +1,28 @@
 { pkgs, lastSupportedVersion, jobsErrors, jobsWarnings }:
 let
-  shared = {
+  files = {
+    # shared files at base level
     base = ./shared/base.nix;
     tokei = ./shared/tokei.nix;
-  };
-  rust = {
-    base = ./rust/base.nix;
-    tests = import ./rust/tests.nix { inherit lastSupportedVersion; };
-    doc = ./rust/doc.nix;
-    miri = ./rust/miri.nix;
-    clippy = ./rust/clippy.nix;
-    machete = ./rust/machete.nix;
-    sort = ./rust/sort.nix;
-  };
-  go = {
-    tests = ./go/tests.nix;
-    gocritic = ./go/gocritic.nix;
-    security_audit = ./go/security_audit.nix;
-  };
-
-  # Lookup table to map strings to imported files
-  lookupTable = {
-    "shared.base" = shared.base;
-    "shared.tokei" = shared.tokei;
-    "rust.base" = rust.base;
-    "rust.tests" = rust.tests;
-    "rust.doc" = rust.doc;
-    "rust.miri" = rust.miri;
-    "rust.clippy" = rust.clippy;
-    "rust.machete" = rust.machete;
-    "rust.sort" = rust.sort;
-    "go.tests" = go.tests;
-    "go.gocritic" = go.gocritic;
-    "go.security_audit" = go.security_audit;
+    
+    # rust files prefixed
+    rust-base = ./rust/base.nix;
+    rust-tests = import ./rust/tests.nix { inherit lastSupportedVersion; };
+    rust-doc = ./rust/doc.nix;
+    rust-miri = ./rust/miri.nix;
+    rust-clippy = ./rust/clippy.nix;
+    rust-machete = ./rust/machete.nix;
+    rust-sort = ./rust/sort.nix;
+    
+    # go files prefixed
+    go-tests = ./go/tests.nix;
+    go-gocritic = ./go/gocritic.nix;
+    go-security_audit = ./go/security_audit.nix;
   };
 
-  # Function to construct jobs based on the lookup table
-  constructJobs = jobNames: pkgs.lib.foldl (acc: jobName: 
-    pkgs.lib.recursiveUpdate acc (import lookupTable.${jobName})
-  ) {} jobNames;
-
+  constructJobs = paths: pkgs.lib.foldl pkgs.lib.recursiveUpdate { } 
+    (map (path: import (builtins.getAttr path files)) paths);
+  
   base = {
     on = {
       push = { };
@@ -47,22 +30,20 @@ let
       workflow_dispatch = { };
     };
   };
-
 in
 {
   errors = (pkgs.formats.yaml { }).generate "" (
     pkgs.lib.recursiveUpdate base {
       name = "Errors";
-      permissions = (import shared.base).permissions;
-      env = (import rust.base).env;
+      permissions = (import files.base).permissions;
+      env = (import files.rust-base).env;
       jobs = constructJobs jobsErrors;
     }
   );
-
   warnings = (pkgs.formats.yaml { }).generate "" (
     pkgs.lib.recursiveUpdate base {
       name = "Warnings";
-      permissions = (import shared.base).permissions;
+      permissions = (import files.base).permissions;
       jobs = constructJobs jobsWarnings;
     }
   );
