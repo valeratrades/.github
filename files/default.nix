b3649f80732a05bf9f0fa8672f588d35967fff92
valeratrades <valeratrades@gmail.com>
2025-02-16 21:04:11 +0000

    fixup! fixup! _

--- files/default.nix
+++ files/default.nix
@@ -8,7 +8,7 @@ in
   licenses = {
     blue_oak = ./licenses/blue_oak.md;
   };
-	preCommit = import ./pre_commit.nix {inherit pkgs;};
+	preCommit = (import ./pre_commit.nix {inherit pkgs;});
   rust = {
     rustfmt = ./rust/rustfmt.nix;
     deny = ./rust/deny.nix;
