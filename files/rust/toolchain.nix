cfe6b0b36624b472098d5eac2c6d732e061f98a5
valeratrades <valeratrades@gmail.com>
2025-02-19 16:30:42 +0000

    _

--- files/rust/toolchain.nix
+++ files/rust/toolchain.nix
@@ -1,8 +1,9 @@
 {
   pkgs,
   toolchain,
+  targets ? [],
 }: (pkgs.formats.toml { }).generate "toolchain.toml" {
   toolchain = {
     channel = "${toolchain}";
-  };
+  } // (if targets != [] then { inherit targets; } else {});
 }
