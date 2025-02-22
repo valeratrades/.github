ec0c7c72957a88ead9acb71fabf487eef20d22a1
valeratrades <valeratrades@gmail.com>
2025-02-22 11:03:05 +0000

    docs: _

--- readme_fw/badges.nix
+++ readme_fw/badges.nix
@@ -15,8 +15,9 @@ let
     docs_rs = ''[<img alt="docs.rs" src="https://img.shields.io/badge/docs.rs-66c2a5?style=for-the-badge&labelColor=555555&logo=docs.rs&style=flat-square" height="20">](https://docs.rs/${pname})'';
     
     loc = ''![Lines Of Code](https://img.shields.io/badge/LoC-${toString loc}-lightblue)'';
-    
-    ci = ''<br>
+   
+		# note that it is possible to remove all references to `master` and have it automatically use the current branch. But that would require running them on `on.push.branches: [**]`.
+		ci = ''<br>
 [<img alt="ci errors" src="https://img.shields.io/github/actions/workflow/status/valeratrades/${pname}/errors.yml?branch=master&style=for-the-badge&style=flat-square&label=errors&labelColor=420d09" height="20">](https://github.com/valeratrades/${pname}/actions?query=branch%3Amaster) <!--NB: Won't find it if repo is private-->
 [<img alt="ci warnings" src="https://img.shields.io/github/actions/workflow/status/valeratrades/${pname}/warnings.yml?branch=master&style=for-the-badge&style=flat-square&label=warnings&labelColor=d16002" height="20">](https://github.com/valeratrades/${pname}/actions?query=branch%3Amaster) <!--NB: Won't find it if repo is private-->'';
   };
