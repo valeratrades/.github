66b2e8b03aecaa8fc4c81dccf1c7925aa88c22ec
valeratrades <valeratrades@gmail.com>
2025-02-27 15:17:08 +0000

    _

--- files/rust/toolchain.nix
+++ files/rust/toolchain.nix
@@ -5,5 +5,6 @@
 }: (pkgs.formats.toml { }).generate "toolchain.toml" {
   toolchain = {
     channel = "${toolchain}";
+		components = "rustc-codegen-cranelift-preview";
   } // (if targets != [] then { inherit targets; } else {});
 }
