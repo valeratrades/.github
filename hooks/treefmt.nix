656879f4e9ef5ee4cdc4e2a146282b549bb6cf85
valeratrades <valeratrades@gmail.com>
2025-03-13 01:20:32 +0000

    _

--- hooks/treefmt.nix
+++ hooks/treefmt.nix
@@ -11,6 +11,7 @@ pkgs,
 		rust = {
 			command = "rustfmt";
 			includes = ["*.rs"];
+			options = ["--edition" "2024"]; # code duplication with formatter in dev-shell, but rust ecosystem is not smart enough yet to avoid it 
 		};
 		# Does not seem to work (treefmt feels VERY raw)
 		rust-leptos = {
