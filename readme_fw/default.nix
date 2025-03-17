bc81ef843e168427360bef2821cf8abe707b7bcb
valeratrades <valeratrades@gmail.com>
2025-03-17 20:08:36 +0000

    good enough

--- readme_fw/default.nix
+++ readme_fw/default.nix
@@ -147,7 +147,7 @@ let
         headerText = if suffixPart == "" then "Installation" else "Installation: ${formatSuffix suffixPart}";
         contentRendered =
           if isMd then
-            ''<pre><code class="markdown-content">${content}</div></pre>'' #HACK: github doesn't support `markdown-content` class. So this is semantically correct but effectively useless.
+            ''<div class="markdown-content">${content}</div>'' #HACK: github doesn't support `markdown-content` class. So this is semantically correct but effectively useless.
           else
             ''<pre><code class="language-sh">${content}</code></pre>'';
       in
