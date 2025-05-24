f7b07041dff76c73da8badabeec4bf39215e5592
Valera <valeratrades@gmail.com>
2025-05-24 19:57:17 +0000

    _

--- files/python/ruff.nix
+++ files/python/ruff.nix
@@ -29,41 +29,33 @@
       "NB"
     ];
     ignore = [
-      "E262"  # no-space-after-inline-comment
-      "E713"
-      "F403"
-      "E401"
-      "E714"  # `${value} is not` instead of `not ${value} is`
-      "E722"
-      "E703"  # don't complain about semicolons
-      "E741"  # Ambiguous var name (I'm a Golang man)
-      "F405"  # Warns when using anything from star imports
-      # Line length regulated by formatter
-      "E501"
-      # pydocstyle: http://www.pydocstyle.org/en/stable/error_codes.html
-      "D401"  # Relax NumPy docstring convention: First line should be in imperative mood
-      # flake8-pytest-style:
-      "PT011" # pytest.raises too broad
-      # flake8-simplify
-      "SIM102" # Use single `if` instead of nested
-      "SIM108" # Use ternary operator
-      # ruff
-      "RUF005" # unpack-instead-of-concatenating-to-collection-literal
-      # pycodestyle
-      # TODO: Remove errors below to further improve docstring linting
-
-      # Ordered from most common to least common errors.
-      "D105"  # Missing docstring in magic method
+			"E261"  # Two spaces before inline comment
+			"E262"  # Inline comment should start with '# '
+			"E265"  # Block comment should start with '# '
+			"E401"  # Multiline imports
       "D100"  # Missing docstring in public module
       "D104"  # Missing docstring in public package
-      # flake8-todos
+      "D105"  # Missing docstring in magic method
+      "D206" # conflicts with ruff formatter
+      "D401"  # Relax NumPy docstring convention: First line should be in imperative mood
+      "E262"  # no-space-after-inline-comment
+      "E401"
+      "E501" # Line length regulated by formatter
+      "E703"  # don't complain about semicolons
+      "E713"
+      "E714"  # `${value} is not` instead of `not ${value} is`
+      "E722"
+      "E741"  # Ambiguous var name (I'm a Golang man)
+      "F403"
+      "F405"  # Warns when using anything from star imports
+      "PT011" # pytest.raises too broad
+      "RUF005" # unpack-instead-of-concatenating-to-collection-literal
+      "SIM102" # Use single `if` instead of nested
+      "SIM108" # Use ternary operator
       "TD002" # Missing author in TODO
       "TD003" # Missing issue link after TODO
-      # tryceratops
       "TRY003" # Avoid long messages outside exception class
-      # Lints below are turned off because of conflicts with the ruff formatter
-      "D206"
-      "W191"
+      "W191" # conflicts with ruff formatter
     ];
   };
 
