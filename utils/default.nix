f1f593c7d8aae1cf065c247268d197641aedc05f
Valera <valeratrades@gmail.com>
2025-08-01 23:51:22 +0000

    utils: env: option to obscure secrets

--- utils/default.nix
+++ utils/default.nix
@@ -1,20 +1,32 @@
+let
+  maskSecret = s: isSecret:
+    if !isSecret then s else
+    let
+      len = builtins.stringLength s;
+    in if len < 8 then "[REDACTED]"
+       else (builtins.substring 0 2 s) + ".." + (builtins.substring (len - 2) 2 s);
+in
 {
-	setDefaultEnv = name: default:
-		''
-						if [ -z "${name}" ]; then
-							export ${name}="${default}"
-							echo "⚠️  [WARN] Default used for ${name} = ${default}"
-						else
-							echo "ℹ️  [INFO] ${name} is set: $'' + "${name}" + ''"
-						fi
-		'';
+  setDefaultEnv = { name, default, is_secret ? false }:
+    let
+      maskedDefault = maskSecret default is_secret;
+    in
+    ''
+      if [ -z "''\${${name}}" ]; then
+        export ${name}="${default}"
+        echo "⚠️  [WARN] Default used for ${name} = ${maskedDefault}"
+      else
+        echo "ℹ️  [INFO] ${name} is set: ${maskSecret "''\${${name}}" is_secret}"
+      fi
+    '';
 
-	requireEnv = name: ''
-						if [ -z "${name}" ]; then
-							echo "❌ [ERROR] Required env ${name} is missing"
-							exit 1
-						else
-							echo "✅ [OK] Required env ${name} is present: $'' + "${name}" + ''"
-						fi
-		'';
+  requireEnv = { name, is_secret ? false }:
+    ''
+      if [ -z "''\${${name}}" ]; then
+        echo "❌ [ERROR] Required env ${name} is missing"
+        exit 1
+      else
+        echo "✅ [OK] Required env ${name} is present: ${maskSecret "''\${${name}}" is_secret}"
+      fi
+    '';
 }
