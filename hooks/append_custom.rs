22c04e3911059d7d8310cf75951a64906cb34536
Valera <valeratrades@gmail.com>
2025-11-03 12:00:24 +0000

    chore: update rust script to new load format

--- hooks/append_custom.rs
+++ hooks/append_custom.rs
@@ -1,3 +1,21 @@
+#!/usr/bin/env nix
+---cargo
+#! nix shell --impure --expr ``
+#! nix let rust_flake = builtins.getFlake ''github:oxalica/rust-overlay'';
+#! nix     nixpkgs_flake = builtins.getFlake ''nixpkgs'';
+#! nix     pkgs = import nixpkgs_flake {
+#! nix       system = builtins.currentSystem;
+#! nix       overlays = [rust_flake.overlays.default];
+#! nix     };
+#! nix     toolchain = pkgs.rust-bin.nightly."2025-10-10".default.override {
+#! nix       extensions = ["rust-src"];
+#! nix     };
+#! nix
+#! nix in toolchain
+#! nix ``
+#! nix --command sh -c ``cargo -Zscript "$0" "$@"``
+---
+
 use std::{
     env,
     fs::{self, OpenOptions},
