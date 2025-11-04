eaf1deed3ba59a1ce730f88c8753bfaa213cd27e
Valera <valeratrades@gmail.com>
2025-11-04 12:49:13 +0000

    _

--- .github/workflows/nix-parts/rust/miri.nix
+++ .github/workflows/nix-parts/rust/miri.nix
@@ -14,7 +14,7 @@
 		}
 		{ run = "cargo miri setup"; }
 		{
-			run = "cargo miri test";
+			run = "cargo miri test --lib --bin"; # normally, `--lib` and `--bin` is exactly what's needed in 99% os cases. If ever run into needing different ones, - add optional flag setting with these as default
 			env.MIRIFLAGS = "-Zmiri-strict-provenance";
 		}
 	];
