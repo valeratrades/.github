35f2a9c952e2de5b93e8c2a9bb03074a189d0092
Valera <valeratrades@gmail.com>
2025-12-31 19:32:17 +0000

    fix: formatting of build.rs

--- files/rust/build.nix
+++ files/rust/build.nix
@@ -1,14 +1,15 @@
 { pkgs, log_directives ? true, git_version ? true }:
 let
-  git_version_code = builtins.readFile ./build/git_version.rs;
-  log_directives_code = builtins.readFile ./build/log_directives.rs;
+  trimEnd = pkgs.lib.strings.trimWith { end = true; };
+  git_version_code = trimEnd (builtins.readFile ./build/git_version.rs);
+  log_directives_code = trimEnd (builtins.readFile ./build/log_directives.rs);
 
   needs_command = git_version;
   use_statement = if needs_command then "use std::process::Command;\n\n" else "";
 
   body_parts = (if git_version then [ git_version_code ] else [])
              ++ (if log_directives then [ log_directives_code ] else []);
-  body = builtins.concatStringsSep "\n" body_parts;
+  body = builtins.concatStringsSep "\n\n" body_parts;
 in
 pkgs.writeText "build.rs" ''
 ${use_statement}fn main() {
