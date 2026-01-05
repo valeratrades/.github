08e772d02648b59f3739d7d74c33b27cdcb498f0
Valera <valeratrades@gmail.com>
2026-01-05 18:10:29 +0000

    bump: _

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@ See individual component descriptions in their respective directories.'';
 
       # Version constants for bundled packages - update these when bumping
       traceyVersion = "1.0.0";
-      codestyleVersion = "0.2.6";
+      codestyleVersion = "0.2.7";
 
       parts = {
         files = (import ./files).description;
