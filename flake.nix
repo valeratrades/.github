259a1bb4a05712dee5fafa88cc808196f3bdbb43
Valera <valeratrades@gmail.com>
2026-01-14 23:23:24 +0000

    fixup! fix: set LD_LIBRARY_PATH for runtime library loading in CI

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@ See individual component descriptions in their respective directories.'';
 
       # Version constants for bundled packages - update these when bumping
       traceyVersion = "1.0.0";
-      codestyleVersion = "0.2.17";
+      codestyleVersion = "0.2.18";
 
       parts = {
         files = (import ./files).description;
