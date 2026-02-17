6a1180e1f4e928c3e543e4698da8a45526ed60d7
Valera <valeratrades@gmail.com>
2026-02-17 02:25:23 +0000

    fixup! fix(pre_ci): remove repo .cargo/config before compiling script

--- github/workflows/nix-parts/rust/clippy.nix
+++ github/workflows/nix-parts/rust/clippy.nix
@@ -16,6 +16,7 @@
 			uses = "actions/download-artifact@v4";
 			"with".name = "modified-cargo-files";
 		}
+    { run = "rm -f .cargo/config.toml .cargo/config"; } # it can have eg `threads=8`; which could lead to races when reasoning about macros
     { run = "cargo clippy --tests -- -Dwarnings"; } #-Dclippy::all #-Dclippy::pedantic
   ];
 }
